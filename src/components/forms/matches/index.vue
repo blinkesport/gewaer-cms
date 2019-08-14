f0032344614e4515ffcd1f58ccd0b16165dc8646
Javier <javier.acevedo233@gmail.com>
2019-08-13 21:48:35 -0400

    WIP

---
+++ src/components/forms/matches/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <form class="resource-form" @submit.prevent="">
+            <div class="row">
+                <div class="col"> 
+                    <h3 class="title"> Matches </h3>
+                </div>
+            </div>
+        </form>
+    </div>
+    
+</template>
+<script>
+
+</script>
+
+<style lang="scss">
+
+</style>
