39061c0765af2e8afb7f2cecd54f5e9d41ee3711
Javier Acevedo <jdacevedog1@gmail.com>
2019-12-13 18:34:35 -0400

    WIP, image in wysiwyg

--- src/components/quill-wrapper/quill-wrapper.vue
+++ src/components/quill-wrapper/quill-wrapper.vue
@@ -51,10 +51,37 @@ export default {
                             "3.0"
                         ]
                     }
+                ],
+                [
+                    {
+                        image: function() {
+                            // let fileInput = this.container.querySelector("input.ql-image[type=file]");
+                            // console.log(fileInput);
+                            // if (fileInput == null) {
+                            //     fileInput = document.createElement("input");
+                            //     fileInput.setAttribute("type", "file");
+                            //     fileInput.setAttribute(
+                            //         "accept",
+                            //         "image/png"
+                            //     );
+                            //     fileInput.classList.add("ql-image");
+                            //     fileInput.addEventListener("change", () => {
+                            //         if (fileInput.files != null && fileInput.files[0] != null) {
+                            //             // Do your own stuff here
+                            //             alert("test");
+                            //             debugger;
+                            //         }
+                            //     });
+                            //     this.container.appendChild(fileInput);
+                            // }
+                            // fileInput.click();
+                        }
+                    }
                 ]
             ],
             editorId: Math.random().toString(16).replace("0.", "q"),
             quill: null
+
         }
     },
     mounted() {
@@ -71,6 +98,16 @@ export default {
                 theme: "snow"
             });
 
+            // this.quill = new Quill(`#${this.editorId}`, {
+            //     modules: {
+            //         toolbar: {
+            //             handlers: {
+            //                 image: this.imageHandler
+            //             }
+            //         }
+            //     },
+            //     theme: "snow"
+            // });
             this.setHtml(this.value);
 
             this.quill.on("text-change", (delta, oldDelta, source) => {
@@ -84,6 +121,9 @@ export default {
                     this.$emit("input", textHTML);
                 }
             });
+
+            const toolbar = this.quill.getModule("toolbar");
+            toolbar.addHandler("image", this.imageHandler);
 
         },
         lineHeightSetup() {
@@ -121,6 +161,37 @@ export default {
             const element = document.createElement("div");
             element.innerHTML = html;
             return element.innerText;
+        },
+        imageHandler() {
+            const input = document.createElement("input");
+            input.setAttribute("type", "file");
+            input.setAttribute("accept", "image/*");
+            input.click();
+            input.onchange = async() => {
+                console.log(this);
+                const file = input.files[0];
+                console.log("User trying to uplaod this:", file);
+
+                // const id = await uploadFile(file); // I'm using react, so whatever upload function
+                const range = this.quill.getSelection();
+                console.log("Range: ", range);
+                // const link = `127.0.0.1:8080/file/${id}`;
+
+                // console.log(file);
+                // axios.post("/filesystem", {
+                //     name: file.name,
+                //     type: file.type,
+                //     files: input.files
+                // }).then((response) => {
+                //     console.log(response);
+                // }).catch((error) => {
+                //     debugger;
+                // });
+
+                // this part the image is inserted
+                // by 'image' option below, you just have to put src(link) of img here.
+                this.quill.container.insertEmbed(range.index, "image", link);
+            }
         }
     }
 }
